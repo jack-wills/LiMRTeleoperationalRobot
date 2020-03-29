53f9128aeee9c3f993df1146cab156d87bf034a6
Jack Williams <jackw53519@gmail.com>
2020-03-29 14:07:22 +0100

    Add upload image code and remove tello drone

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
+import requests
+import datetime
+import json
 
-def main:
+def uploadImage(imageX, imageY, imageZ, rotation):
+    data = {"timestamp": datetime.datetime.now().isoformat(),
+            "imageX": imageX,
+            "imageY": imageY,
+            "imageZ": imageZ,
+            "rotation": rotation } 
+
+    url = "http://bmgxwpyyd2.execute-api.us-east-1.amazonaws.com/prod/uploadimage"
+
+    http_response = requests.post(url, json=data)
+
+
+    url = json.loads(http_response.text)["URL"]
+
+    http_response = requests.put(url, data=open('./test.png', 'rb'))
+    print(http_response.text)
+
+def main():
     print("Hello World!")
 
 if __name__ == "__main__":
