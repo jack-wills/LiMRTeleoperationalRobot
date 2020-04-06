0c34c71d7f76f2b02daa5f2f0a85b3db9b8cfe4f
Jack Williams <jackw53519@gmail.com>
2020-04-06 01:47:17 +0100

    Collating robot code. Fixed an issue with astar path finding

--- src/main.py
+++ src/main.py
@@ -6,14 +6,123 @@ import time
 import socket
 import pyaudio
 
+import astar
+
 url_prefix = "http://bmgxwpyyd2.execute-api.us-east-1.amazonaws.com/prod/"
 
 def main():
-    audioThread = threading.Thread(target=audioThreadFunction, args=(1,))
+    audioThread = threading.Thread(target=audioThreadFunction, daemon=True)
     audioThread.start()
 
+    url = url_prefix + "sensordata"
+    maze = [[1 for j in range(100)] for i in range(100)]
+
+    for x in range(100):
+        for y in range(100):
+            if(31 <= x <= 39 or 70 <= x <= 77 or 55 <= y <= 64 or 25 <= y <= 34):
+                maze[x][y] = 0
+    
+    lastGoToX = 0
+    lastGoToY = 0
+    currentX = 0
+    currentY = 0
+    pathFollowThread = 0
+
+    response_cloud = requests.get(url)
+    sensorValues = json.loads(response_cloud.json()['sensorValues'])
+    
+    lastDroneImageX = sensorValues['droneImageX']
+    lastDroneImageY = sensorValues['droneImageY']
+    lastDroneImageZ = sensorValues['droneImageZ']
+    lastDroneImageRot = sensorValues['droneImageRot']
     while 1:
-        print("hello world")
+        response_cloud = requests.get(url)
+
+        sensorValues = json.loads(response_cloud.json()['sensorValues'])
+
+        currentX = round(translate(float(sensorValues['robotCoordX']), -5.0, 8.0, 1.0, 100.0))
+        currentY = round(translate(float(sensorValues['robotCoordY']), 3.0, 13.0, 1.0, 100.0))
+        goToX = round(translate(float(sensorValues['robotGoToX']), -5.0, 8.0, 1.0, 100.0))
+        goToY = round(translate(float(sensorValues['robotGoToY']), 3.0, 13.0, 1.0, 100.0))
+
+        if ((currentX != goToX or currentY != goToY) and (lastGoToX != goToX or lastGoToY != goToY)):
+            if (pathFollowThread != 0 and pathFollowThread.is_alive()):
+                pathFollowThread.stop = True
+                while(pathFollowThread.is_alive()):
+                    time.sleep(0.01)
+            lastGoToX = goToX
+            lastGoToY = goToY
+            start = (currentX, currentY)
+            end = (goToX, goToY)
+
+            path = astar.astar(maze, start, end)
+            print(path)
+
+            pathFollowThread = threading.Thread(target=pathFollowFunction, args=(path,))
+            pathFollowThread.start()
+        
+        droneImageX = sensorValues['droneImageX']
+        droneImageY = sensorValues['droneImageY']
+        droneImageZ = sensorValues['droneImageZ']
+        droneImageRot = sensorValues['droneImageRot']
+        if (lastDroneImageX != droneImageX or lastDroneImageY != droneImageY or lastDroneImageZ != droneImageZ or lastDroneImageRot != droneImageRot):
+            lastDroneImageX = droneImageX
+            lastDroneImageY = droneImageY
+            lastDroneImageZ = droneImageZ
+            lastDroneImageRot = droneImageRot
+            uploadImage(droneImageX, droneImageY, droneImageZ, droneImageRot)
+        time.sleep(1)
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+def pathFollowFunction(path):
+    t = threading.currentThread()
+    url = url_prefix + "sensordata"
+    for i in range(len(path)-1):
+        if (i%3 != 0):
+            continue
+        coord = path[i]
+        if (getattr(t, "stop", False)):
+            print("stopping")
+            return
+        print(coord)
+        coord_current_X = translate(coord[0], 1.0, 100.0, -5.0, 8.0)
+        coord_current_Y = translate(coord[1], 1.0, 100.0, 3.0, 13.0)
+        
+        coords = {"robotCoordX": str(coord_current_X),
+                  "robotCoordY": str(coord_current_Y)}
+        
+        data = {
+            "timestamp": "{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now()),
+            "sensorValues": json.dumps(coords)
+        }
+
+        requests.post(url, json=data)
+
+    coord = path[-1]
+    print(coord)
+    coord_current_X = translate(coord[0], 1.0, 100.0, -5.0, 8.0)
+    coord_current_Y = translate(coord[1], 1.0, 100.0, 3.0, 13.0)
+    
+    coords = {"robotCoordX": str(coord_current_X),
+                "robotCoordY": str(coord_current_Y)}
+    
+    data = {
+        "timestamp": "{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now()),
+        "sensorValues": json.dumps(coords)
+    }
+
+    requests.post(url, json=data)
+
 
 def uploadImage(imageX, imageY, imageZ, rotation):
     data = {"timestamp": datetime.datetime.now().isoformat(),
